11342d59cbf44a8a45b016ced6cb08513315bddb
changxianglin <281532612@qq.com>
2021-01-07 01:56:29 +0800

    init project

---
+++ main.js
@@ -0,0 +1,23 @@
+for (let i of [1, 2, 3]) {
+  console.log(i)
+}
+
+function createElement(tagName, attributes, ...children) {
+  let e = document.createElement(tagName)
+  for (const p in attributes) {
+    e.setAttribute(p, attributes[p])
+  }
+  for (const child of children) {
+    if (typeof child === 'string') {
+      child = document.createTextNode(child)
+    }
+    e.appendChild(child)
+  }
+  return e
+}
+
+document.body.appendChild(<div id="a" class="c" >
+  <div>abc</div>
+  <div></div>
+  <div></div>
+</div>)
