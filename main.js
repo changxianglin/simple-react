675602df9d1d26fa58bc5c5d15e31b11dfeb3457
changxianglin <281532612@qq.com>
2021-03-23 01:22:48 +0800

    rerender

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 import { createElement, Component, render } from './toy-react.js'
 
 class Mycomponent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      a: 1,
+      b: 2
+    }
+  }
   render() {
     return <div>
       <h1>my component</h1>
-      {this.children}
+      <button onclick={() => {this.state.a ++; this.rerender(); }}>add</button>
+      <span>{this.state.a.toString()}</span>
+      {/* {this.children} */}
     </div>
   }
 }
